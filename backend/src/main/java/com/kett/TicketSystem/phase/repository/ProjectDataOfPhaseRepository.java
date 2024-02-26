f680a5498f73f7c5a3a820081f4b540ba248e7cf
dkettner <kettner_dennis@web.de>
2024-02-26 21:09:16 +0100

    chore(phase): deleted unused method from repository interface

--- backend/src/main/java/com/kett/TicketSystem/phase/repository/ProjectDataOfPhaseRepository.java
+++ backend/src/main/java/com/kett/TicketSystem/phase/repository/ProjectDataOfPhaseRepository.java
@@ -4,11 +4,11 @@ import com.kett.TicketSystem.phase.domain.consumedData.ProjectDataOfPhase;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ProjectDataOfPhaseRepository extends JpaRepository<ProjectDataOfPhase, UUID> {
-    ProjectDataOfPhase findByProjectId(UUID projectId);
     Integer deleteByProjectId(UUID projectId);
 
     Boolean existsByProjectId(UUID projectId);
